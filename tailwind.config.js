9a89566e8d01125b0d9cd6f481dcc3264ded9786
Muslim Mac <alturaabgroup@gmail.com>
2025-12-25 18:01:20 +0530

    remove old code

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      colors: { brand: { DEFAULT: "#0F766E" } }
-    }
-  },
-  plugins: []
-}
